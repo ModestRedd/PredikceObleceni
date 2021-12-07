91f7f64cf367cb2c2cdadd1a9a4c8323b803a935
Jiří Matějka <matj27@vse.cz>
2021-12-07 14:46:49 +0100

    Zapis dat ulozenych v databazi do .json souboru funkcni. Realizovano ve tride Persistance. Zaroven existuje roztrideni obleceni podle umisteni na tele. K temto se da ze tridy Persistance dostat pres gettery

--- src/main/java/cz/vse/si/predikceobleceni/main/Persistence.java
+++ src/main/java/cz/vse/si/predikceobleceni/main/Persistence.java
@@ -3,22 +3,37 @@ package cz.vse.si.predikceobleceni.main;
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
-import cz.vse.si.predikceobleceni.model.obleceni.Obleceni;
+import cz.vse.si.predikceobleceni.model.obleceni.*;
 import cz.vse.si.predikceobleceni.model.svet.Casoprostor;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Persistence {
-
     private ArrayList<Obleceni> obleceni = new ArrayList<>();
     private ArrayList<Casoprostor> lokality = new ArrayList<>();
 
-    Persistence(String obleceniJson, String lokalityJson) {
+    boolean jsouSeznamyObleceniAktualni = true;
+    private ArrayList<Cepice> hlava = new ArrayList<Cepice>();
+    private ArrayList<Vrsek> vrsek = new ArrayList<Vrsek>();
+    private ArrayList<Spodek> spodek = new ArrayList<Spodek>();
+    private ArrayList<Boty> boty = new ArrayList<Boty>();
+
+    Persistence(String pathObleceniJson, String pathLokalityJson) throws IOException {
+        String obleceniJson = Files.readString(Path.of(pathObleceniJson), StandardCharsets.UTF_8);
+        String lokalityJson = Files.readString(Path.of(pathLokalityJson), StandardCharsets.UTF_8);
+
         pridejObleceni(obleceniJson);
         pridejLokality(lokalityJson);
     }
 
     protected void pridejObleceni(String obleceniJson) {
+        jsouSeznamyObleceniAktualni = false;
+
         Gson gson = new Gson();
 
         try {
@@ -33,15 +48,43 @@ public class Persistence {
                     obleceniProPridani.get(i).setId(obleceniSize + i);
                 }
 
+                zaradObleceni(obleceniProPridani);
+                jsouSeznamyObleceniAktualni = true;
+
                 obleceni.addAll(obleceniProPridani);
 
             } else {
                 Obleceni obleceniProPridani = gson.fromJson(obleceniJson, Obleceni.class);
                 obleceniProPridani.setId(obleceni.size());
+
+                zaradObleceni(obleceniProPridani);
+                jsouSeznamyObleceniAktualni = true;
+
                 obleceni.add(obleceniProPridani);
             }
         } catch (JsonParseException exception) {
             System.out.println("ERROR: Zadany JSON s daty obleceni je nevalidni! Exception: " + exception);
+        }
+    }
+
+    private void zaradObleceni(ArrayList<Obleceni> obleceniKZarazeni) {
+        obleceniKZarazeni.forEach(this::zaradObleceni);
+    }
+
+    private void zaradObleceni(Obleceni obleceniKZarazeni) {
+        switch (obleceniKZarazeni.getCastTela()) {
+            case HLAVA -> {
+                this.hlava.add((Cepice) obleceniKZarazeni);
+            }
+            case TELO -> {
+                this.vrsek.add((Vrsek) obleceniKZarazeni);
+            }
+            case NOHY -> {
+                this.spodek.add((Spodek) obleceniKZarazeni);
+            }
+            case BOTY -> {
+                this.boty.add((Boty) obleceniKZarazeni);
+            }
         }
     }
 
@@ -78,9 +121,66 @@ public class Persistence {
         return gson.toJson(obleceni);
     }
 
+    protected void dumpObleceniJson(String pathToFile) throws IOException {
+        String json = dumpObleceniJson();
+
+        Files.write(Path.of(pathToFile), List.of(json), StandardCharsets.UTF_8);
+    }
+
     protected String dumpLokalityJson() {
         Gson gson = new Gson();
 
         return gson.toJson(lokality);
+    }
+
+    protected void dumpLokalityJson(String pathToFile) throws IOException {
+        String json = dumpLokalityJson();
+
+        Files.write(Path.of(pathToFile), List.of(json), StandardCharsets.UTF_8);
+    }
+
+    public ArrayList<Casoprostor> getLokality() {
+        return lokality;
+    }
+
+    private void zaradVeskereObleceni(){
+        hlava = new ArrayList<Cepice>();
+        vrsek = new ArrayList<Vrsek>();
+        spodek = new ArrayList<Spodek>();
+        boty = new ArrayList<Boty>();
+        zaradObleceni(obleceni);
+        jsouSeznamyObleceniAktualni = true;
+    }
+
+    public ArrayList<Cepice> getHlava() {
+        if (!jsouSeznamyObleceniAktualni) {
+            zaradVeskereObleceni();
+        }
+
+        return hlava;
+    }
+
+    public ArrayList<Vrsek> getVrsek() {
+        if (!jsouSeznamyObleceniAktualni) {
+            zaradVeskereObleceni();
+        }
+
+        return vrsek;
+    }
+
+    public ArrayList<Spodek> getSpodek() {
+        if (!jsouSeznamyObleceniAktualni) {
+            zaradVeskereObleceni();
+        }
+
+        return spodek;
+    }
+
+    public ArrayList<Boty> getBoty() {
+        if (!jsouSeznamyObleceniAktualni) {
+            zaradVeskereObleceni();
+        }
+        
+        return boty;
     }
 }
--- src/main/java/cz/vse/si/predikceobleceni/main/Start.java
+++ src/main/java/cz/vse/si/predikceobleceni/main/Start.java
@@ -1,24 +1,14 @@
 package cz.vse.si.predikceobleceni.main;
 
-import com.google.gson.Gson;
-import cz.vse.si.predikceobleceni.model.obleceni.*;
-import cz.vse.si.predikceobleceni.model.svet.Casoprostor;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Start {
 
     public static void main(String[] args) throws IOException {
-
-        String obleceniJson = Files.readString(Path.of("example_data/ObleceniDefault.json"), StandardCharsets.UTF_8);
-        String lokalityJson = Files.readString(Path.of("example_data/LokalityDefault.json"), StandardCharsets.UTF_8);
-
-        Persistence persistence = new Persistence(obleceniJson, lokalityJson);
+        Persistence persistence = new Persistence("example_data/ObleceniDefault.json", "example_data/LokalityDefault.json");
 
         persistence.pridejObleceni(Files.readString(Path.of("example_data/ObleceniJedno.json"), StandardCharsets.UTF_8));
         persistence.pridejLokality(Files.readString(Path.of("example_data/LokalitaJedna.json"), StandardCharsets.UTF_8));
@@ -26,63 +16,7 @@ public class Start {
         persistence.pridejObleceni(Files.readString(Path.of("example_data/ObleceniPole.json"), StandardCharsets.UTF_8));
         persistence.pridejLokality(Files.readString(Path.of("example_data/LokalityPole.json"), StandardCharsets.UTF_8));
 
-        System.out.println("OBLECENI:\n" + persistence.dumpObleceniJson());
-        System.out.println("LOKALITY:\n" + persistence.dumpLokalityJson());
-
-        /*
-        Obleceni mikina = new Vrsek("Mikina", Vrstva.DRUHA, 10, 20, Formalni.MALO);
-        System.out.println("Pouze toString(): " + mikina);
-        Obleceni kalhoty = new Spodek("Kalhoty", Vrstva.PRVNI, -10, 40, Formalni.HODNE);
-        System.out.println("Pouze toString(): " + kalhoty);
-        Casoprostor les = new Casoprostor("Les", 50.123, 13.240, 1638822222, 1638832222, List.of(Formalni.MALO));
-        System.out.println("Pouze toString(): " + les);
-        Casoprostor skola = new Casoprostor("Skola", 50.456, 14.987, 1531234567, 1636665557, List.of(Formalni.MALO, Formalni.STREDNE));
-        System.out.println("Pouze toString(): " + skola);
-
-        System.out.println("--------------------------------------------------");
-
-        ArrayList<Obleceni> obleceni = new ArrayList<>();
-        obleceni.add(mikina);
-        obleceni.add(kalhoty);
-
-        Gson g = new Gson();
-
-        System.out.println("JSONARRAY: " + g.toJson(obleceni));
-
-        ArrayList<Obleceni> obleceni2 = new ArrayList<>();
-
-        System.out.println("--------------------------------------------------");
-
-
-
-
-        String mikinaJson = g.toJson(mikina);
-        System.out.println("JSON: " + mikinaJson);
-        String kalhotyJson = g.toJson(kalhoty);
-        System.out.println("JSON: " + kalhotyJson);
-        String lesJson = g.toJson(les);
-        System.out.println("JSON: " + lesJson);
-        String skolaJson = g.toJson(skola);
-        System.out.println("JSON: " + skolaJson);
-
-        System.out.println("--------------------------------------------------");
-
-        Obleceni mikinaGson = g.fromJson(mikinaJson, Obleceni.class);
-        System.out.println("Imported from JSON: " + mikinaGson);
-        Obleceni kalhotyGson = g.fromJson(kalhotyJson, Obleceni.class);
-        System.out.println("Imported from JSON: " + kalhotyGson);
-        Casoprostor lesGson = g.fromJson(lesJson, Casoprostor.class);
-        System.out.println("Imported from JSON: " + lesGson);
-        Casoprostor skolaGson = g.fromJson(skolaJson, Casoprostor.class);
-        System.out.println("Imported from JSON: " + skolaGson);
-
-
-        /*
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
-        String jsonString = gson.toJson(mikina);
-        System.out.println(jsonString);
-         */
+        persistence.dumpObleceniJson("data/Obleceni.json");
+        persistence.dumpLokalityJson("data/Lokality.json");
     }
 }
