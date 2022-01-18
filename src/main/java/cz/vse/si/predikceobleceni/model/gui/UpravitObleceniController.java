cce4af23e5782db9b29d828a2c4dc05ea1612188
Jiří Matějka <matj27@vse.cz>
2022-01-18 15:06:12 +0100

    upravit obleceni in progress

---
+++ src/main/java/cz/vse/si/predikceobleceni/model/gui/UpravitObleceniController.java
@@ -0,0 +1,12 @@
+package cz.vse.si.predikceobleceni.model.gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
+public class UpravitObleceniController {
+    public ComboBox castTela;
+    public ComboBox vrstva;
+
+    public void ulozitObleceni(ActionEvent actionEvent) {
+    }
+}
