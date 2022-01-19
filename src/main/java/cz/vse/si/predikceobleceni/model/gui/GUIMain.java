ea9b91ac69e25902a2c5b3d493d1ecf0b8e12c8e
Václav Jochim <jocv00@vse.cz>
2022-01-19 11:16:28 +0100

    přidána ikonka a úprava stylu tlačítek

--- src/main/java/cz/vse/si/predikceobleceni/model/gui/GUIMain.java
+++ src/main/java/cz/vse/si/predikceobleceni/model/gui/GUIMain.java
@@ -5,10 +5,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Objects;
 
 
@@ -24,7 +27,9 @@ public class GUIMain extends Application {
         //Parent root = loader.load(getClass().getClassLoader().getResource("gui.fxml"));
 
         Scene scene = new Scene(root);
+        Path path = FileSystems.getDefault().getPath("./src/main/java/cz/vse/si/predikceobleceni/model/resources/clouds.png");
 
+        stage.getIcons().add(new Image("file:" + path.toAbsolutePath()));
         stage.setTitle("Předpověď oblečení");
         stage.setScene(scene);
         stage.show();
